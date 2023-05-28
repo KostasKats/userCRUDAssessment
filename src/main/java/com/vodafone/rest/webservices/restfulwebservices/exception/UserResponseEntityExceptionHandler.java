76f822309c12543184aa3e366e93b989a5c9673c
kostas <kkatsaros28@gmail.com>
2023-05-28 21:33:22 +0300

    comment unused exception

--- src/main/java/com/vodafone/rest/webservices/restfulwebservices/exception/UserResponseEntityExceptionHandler.java
+++ src/main/java/com/vodafone/rest/webservices/restfulwebservices/exception/UserResponseEntityExceptionHandler.java
@@ -20,13 +20,13 @@ public class UserResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
+/*    @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    }*/
 
 
 }
